3ff7ee1e88802b98f383451aeb787536bfc88128
kiwiskewer <lizhang.sc@gmail.com>
2016-04-24 19:48:58 -0700

    temp

--- ListComb.py
+++ ListComb.py
@@ -1,4 +1,4 @@
-def listComb(str):
+def listComb1(str):
     for n in range(1,1<<len(str)):
         s = ''
         i = 0
@@ -9,7 +9,17 @@ def listComb(str):
             n >>= 1
         print (s)
 
-tests = 'abcde'
+tests = 'abc'
 
-listComb(tests)
+#listComb1(tests)
 
+def listComb2(comb, str, l, r):
+
+    for i in range(l,r+1):
+        comb += str[i]
+        print(comb)
+        listComb2(comb, str, i+1, r)
+        comb = comb[:-1]
+
+sol = ''
+listComb2(sol, tests, 0, len(tests)-1)
